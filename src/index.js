e725e4ef142ffa68bd2c9444cbbf2e067bfcbd7b
Omarashraf2002 <omarash2020@gmail.com>
2023-09-17 03:54:44 +0300

    COMMIT & PUSH

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Header from "./Components/Header/Header";
+import TvShow from "./Components/TVShow/TvShow";
+import TvDetails from "./Components/tvshowdetails/TvDetails";
+import Signup from "./Components/Signup/Signup";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Signup />} />
+      <Route path="/tv" element={<TvShow />} />
+      <Route path="/tvdetails/:id" element={<TvDetails />} />
+    </Routes>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
